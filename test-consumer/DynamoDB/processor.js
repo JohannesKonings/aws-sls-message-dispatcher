b64ba29c642cffe8d58721d89e85754ca540a617
Johannes Konings <johannes.konings@outlook.com>
2021-01-03 19:37:26 +0100

    connection to dynamodb added

--- test-consumer/DynamoDB/processor.js
+++ test-consumer/DynamoDB/processor.js
@@ -3,6 +3,32 @@ const AWS = require('aws-sdk');
 
 module.exports.processTopic2 = async event => {
 
+  console.log("region", process.env.region);
+  console.log("tableName", process.env.tableName);
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const messageString = event.Records[0].body;
+  const messageJson = JSON.parse(messageString);
+  const timestamp = new Date().toISOString();
+
+  const eventData = {
+    pk: "topic2",
+    sk: timestamp,
+    message: messageJson.Message,
+  };
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: eventData,
+  };
+
+  await dynamoDb.put(params).promise().then(function (data) {
+    console.log('put to dynamoDB succesfull');
+  }, function (err) {
+    console.error("put to dynamoDB", err);
+  })
+
   return {
     statusCode: 200,
     body: JSON.stringify(
